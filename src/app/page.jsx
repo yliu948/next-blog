38448b5e0fac56a7eb137c9e6fecbae71a14b4d0
Yi Liu <justinliuyi@gmail.com>
2023-09-14 16:52:45 -0400

    init update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,47 @@
 import styles from "./homepage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href="/">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quasi
+          voluptatum natus impedit alias fuga rerum animi perferendis non velit
+          eaque sint nostrum consequuntur pariatur fugiat voluptas voluptate
+          explicabo illo.
+        </h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        blanditiis facere fuga, nisi illum corrupti praesentium veniam! Eveniet,
+        voluptas beatae. Voluptate, quas. Repudiandae consequuntur assumenda,
+        earum nesciunt necessitatibus voluptatem mollitia! blanditiis facere
+        fuga, nisi illum corrupti praesentium veniam! Eveniet, voluptas beatae.
+        Voluptate, quas. Repudiandae consequuntur assumenda, earum nesciunt
+        necessitatibus voluptatem mollitia! blanditiis facere fuga, nisi illum
+        corrupti praesentium veniam! Eveniet, voluptas beatae. Voluptate, quas.
+        Repudiandae consequuntur assumenda, earum nesciunt necessitatibus
+        voluptatem mollitia! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Quibusdam blanditiis facere fuga, nisi illum corrupti praesentium
+        veniam! Eveniet, voluptas beatae. Voluptate, quas. Repudiandae
+        consequuntur assumenda, earum nesciunt necessitatibus voluptatem
+        mollitia! blanditiis facere fuga, nisi illum corrupti praesentium
+        veniam! Eveniet, voluptas beatae. Voluptate, quas. Repudiandae
+        consequuntur assumenda, earum nesciunt necessitatibus voluptatem
+        mollitia! blanditiis facere fuga, nisi illum corrupti praesentium
+        veniam! Eveniet, voluptas beatae. Voluptate, quas. Repudiandae
+        consequuntur assumenda, earum nesciunt necessitatibus voluptatem
+        mollitia! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Quibusdam blanditiis facere fuga, nisi illum corrupti praesentium
+        veniam! Eveniet, voluptas beatae. Voluptate, quas. Repudiandae
+        consequuntur assumenda, earum nesciunt necessitatibus voluptatem
+        mollitia! blanditiis facere fuga, nisi illum corrupti praesentium
+        veniam! Eveniet, voluptas beatae. Voluptate, quas. Repudiandae
+        consequuntur assumenda, earum nesciunt necessitatibus voluptatem
+        mollitia! blanditiis facere fuga, nisi illum corrupti praesentium
+        veniam! Eveniet, voluptas beatae. Voluptate, quas. Repudiandae
+        consequuntur assumenda, earum nesciunt necessitatibus voluptatem
+        mollitia!
+      </Link>
+    </div>
+  );
 }
