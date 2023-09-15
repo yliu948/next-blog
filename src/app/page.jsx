ee2c49dda426fb0b9467fe60eee10a37f5bc63b0
Yi Liu <justinliuyi@gmail.com>
2023-09-15 00:09:37 -0400

    updated theme switch

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,47 +1,18 @@
+import Featured from "@/components/featured/Featured";
 import styles from "./homepage.module.css";
-import Link from "next/link";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/">
-        <h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quasi
-          voluptatum natus impedit alias fuga rerum animi perferendis non velit
-          eaque sint nostrum consequuntur pariatur fugiat voluptas voluptate
-          explicabo illo.
-        </h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-        blanditiis facere fuga, nisi illum corrupti praesentium veniam! Eveniet,
-        voluptas beatae. Voluptate, quas. Repudiandae consequuntur assumenda,
-        earum nesciunt necessitatibus voluptatem mollitia! blanditiis facere
-        fuga, nisi illum corrupti praesentium veniam! Eveniet, voluptas beatae.
-        Voluptate, quas. Repudiandae consequuntur assumenda, earum nesciunt
-        necessitatibus voluptatem mollitia! blanditiis facere fuga, nisi illum
-        corrupti praesentium veniam! Eveniet, voluptas beatae. Voluptate, quas.
-        Repudiandae consequuntur assumenda, earum nesciunt necessitatibus
-        voluptatem mollitia! Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Quibusdam blanditiis facere fuga, nisi illum corrupti praesentium
-        veniam! Eveniet, voluptas beatae. Voluptate, quas. Repudiandae
-        consequuntur assumenda, earum nesciunt necessitatibus voluptatem
-        mollitia! blanditiis facere fuga, nisi illum corrupti praesentium
-        veniam! Eveniet, voluptas beatae. Voluptate, quas. Repudiandae
-        consequuntur assumenda, earum nesciunt necessitatibus voluptatem
-        mollitia! blanditiis facere fuga, nisi illum corrupti praesentium
-        veniam! Eveniet, voluptas beatae. Voluptate, quas. Repudiandae
-        consequuntur assumenda, earum nesciunt necessitatibus voluptatem
-        mollitia! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Quibusdam blanditiis facere fuga, nisi illum corrupti praesentium
-        veniam! Eveniet, voluptas beatae. Voluptate, quas. Repudiandae
-        consequuntur assumenda, earum nesciunt necessitatibus voluptatem
-        mollitia! blanditiis facere fuga, nisi illum corrupti praesentium
-        veniam! Eveniet, voluptas beatae. Voluptate, quas. Repudiandae
-        consequuntur assumenda, earum nesciunt necessitatibus voluptatem
-        mollitia! blanditiis facere fuga, nisi illum corrupti praesentium
-        veniam! Eveniet, voluptas beatae. Voluptate, quas. Repudiandae
-        consequuntur assumenda, earum nesciunt necessitatibus voluptatem
-        mollitia!
-      </Link>
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
     </div>
   );
 }
---
+++ src/providers/ThemeProvider.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ThemeContext } from "@/context/ThemeContext";
+import React, { useContext, useEffect, useState } from "react";
+
+export const ThemeProvider = ({ children }) => {
+  const { theme } = useContext(ThemeContext);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (mounted) {
+    return <div className={theme}>{children}</div>;
+  }
+};
+
+export default ThemeProvider;
