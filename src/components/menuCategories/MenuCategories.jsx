5cd4dde0a030c9466c3e2edae47436a9e3cfe485
Yi Liu <justinliuyi@gmail.com>
2024-02-16 13:08:20 -0500

    update

--- src/components/menuCategories/MenuCategories.jsx
+++ src/components/menuCategories/MenuCategories.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import Link from "next/link";
 import styles from "./menuCategories.module.css";
 import prisma from "@/utils/connect";
+import { NEXT_URL } from "@/utils/config";
 
-// const getData = async () => {
-//   const res = await fetch("http://localhost:3000/api/categories", {
-//     cache: "no-store",
-//   });
-//   if (!res.ok) {
-//     throw new Error("Failed");
-//   }
-//   return res.json();
-// };
+const getData = async () => {
+  const res = await fetch(`${NEXT_URL}/api/categories`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+  return res.json();
+};
 
 const MenuCategories = async () => {
   //const data = await getData();
-  const data = await prisma.category.findMany();
+  //const data = await prisma.category.findMany();
   return (
     <div className={styles.categoryList}>
-      {data?.map((item) => (
+      {/* {data?.map((item) => (
         <Link
           href={`/blog?cat=${item.slug}`}
           className={`${styles.categoryItem} ${styles[item.slug]}`}
@@ -26,7 +27,7 @@ const MenuCategories = async () => {
         >
           {item.title}
         </Link>
-      ))}
+      ))} */}
     </div>
   );
 };
