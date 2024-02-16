637981f29b253cb52ee9e151eb70bda3978c6fc0
Yi Liu <justinliuyi@gmail.com>
2024-02-16 12:54:43 -0500

    update

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -23,7 +23,7 @@ const SinglePage = async ({ params }) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.infoContainer}>
+      {/* <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
           <h1 className={styles.title}>{data?.title}</h1>
           <div className={styles.user}>
@@ -60,7 +60,7 @@ const SinglePage = async ({ params }) => {
           </div>
         </div>
         <Menu />
-      </div>
+      </div> */}
     </div>
   );
 };
