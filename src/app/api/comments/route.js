f451446b6dacd2deb43cdec51c3ada46100243ca
Yi Liu <justinliuyi@gmail.com>
2024-02-16 10:29:19 -0500

    update

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -2,28 +2,28 @@ import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-// GET ALL COMMENTS OF A POST
-export const GET = async (req) => {
-  const { searchParams } = new URL(req.url);
+// // GET ALL COMMENTS OF A POST
+// export const GET = async (req) => {
+//   const { searchParams } = new URL(req.url);
 
-  const postSlug = searchParams.get("postSlug");
-  //console.log(postSlug);
-  try {
-    const comments = await prisma.comment.findMany({
-      where: {
-        ...(postSlug && { postSlug }),
-      },
-      include: { user: true },
-    });
+//   const postSlug = searchParams.get("postSlug");
+//   //console.log(postSlug);
+//   try {
+//     const comments = await prisma.comment.findMany({
+//       where: {
+//         ...(postSlug && { postSlug }),
+//       },
+//       include: { user: true },
+//     });
 
-    return new NextResponse(JSON.stringify(comments, { status: 200 }));
-  } catch (err) {
-    // console.log(err);
-    return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
-    );
-  }
-};
+//     return new NextResponse(JSON.stringify(comments, { status: 200 }));
+//   } catch (err) {
+//     // console.log(err);
+//     return new NextResponse(
+//       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+//     );
+//   }
+// };
 
 // create a comment
 export const POST = async (req) => {
--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -2,36 +2,36 @@ import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-// get multiple posts
-export const GET = async (req) => {
-  const { searchParams } = new URL(req.url);
+// // get multiple posts
+// export const GET = async (req) => {
+//   const { searchParams } = new URL(req.url);
 
-  const page = parseInt(searchParams.get("page"));
-  const cat = searchParams.get("cat");
+//   const page = parseInt(searchParams.get("page"));
+//   const cat = searchParams.get("cat");
 
-  const POST_PER_PAGE = 2;
+//   const POST_PER_PAGE = 2;
 
-  const query = {
-    take: POST_PER_PAGE,
-    skip: POST_PER_PAGE * (page - 1),
-    where: {
-      ...(cat && { catSlug: cat }),
-    },
-  };
+//   const query = {
+//     take: POST_PER_PAGE,
+//     skip: POST_PER_PAGE * (page - 1),
+//     where: {
+//       ...(cat && { catSlug: cat }),
+//     },
+//   };
 
-  try {
-    const [posts, count] = await prisma.$transaction([
-      prisma.post.findMany(query),
-      prisma.post.count({ where: query.where }),
-    ]);
-    return new NextResponse(JSON.stringify({ posts, count }, { status: 200 }));
-  } catch (err) {
-    console.log(err);
-    return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
-    );
-  }
-};
+//   try {
+//     const [posts, count] = await prisma.$transaction([
+//       prisma.post.findMany(query),
+//       prisma.post.count({ where: query.where }),
+//     ]);
+//     return new NextResponse(JSON.stringify({ posts, count }, { status: 200 }));
+//   } catch (err) {
+//     console.log(err);
+//     return new NextResponse(
+//       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+//     );
+//   }
+// };
 
 // create a post
 export const POST = async (req) => {
