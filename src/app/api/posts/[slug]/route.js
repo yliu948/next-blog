84e62a2e307e1ca19ea7384191d72532969a8f16
Yi Liu <justinliuyi@gmail.com>
2024-02-16 10:36:48 -0500

    update

--- src/app/api/posts/[slug]/route.js
+++ src/app/api/posts/[slug]/route.js
@@ -1,23 +1,23 @@
-// import { getAuthSession } from "@/utils/auth";
-// import prisma from "@/utils/connect";
-// import { NextResponse } from "next/server";
+import { getAuthSession } from "@/utils/auth";
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
 
-// // Get single post
-// export const GET = async (req, { params }) => {
-//   const { slug } = params;
+// Get single post
+export const GET = async (req, { params }) => {
+  const { slug } = params;
 
-//   try {
-//     const post = await prisma.post.update({
-//       where: { slug },
-//       data: { views: { increment: 1 } },
-//       include: { user: true },
-//     });
+  try {
+    const post = await prisma.post.update({
+      where: { slug },
+      data: { views: { increment: 1 } },
+      include: { user: true },
+    });
 
-//     return new NextResponse(JSON.stringify(post, { status: 200 }));
-//   } catch (err) {
-//     console.log(err);
-//     return new NextResponse(
-//       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
-//     );
-//   }
-// };
+    return new NextResponse(JSON.stringify(post, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
