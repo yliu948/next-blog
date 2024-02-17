3618fb937daff76e436c132b8732e13162c9fbed
Yi Liu <justinliuyi@gmail.com>
2024-02-17 00:50:10 -0500

    update

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -96,61 +96,63 @@ const WritePage = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <input
-        type="text"
-        placeholder="Title"
-        className={styles.input}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <select
-        className={styles.select}
-        onChange={(e) => setCatSlug(e.target.value)}
-      >
-        <option value="style">style</option>
-        <option value="fashion">fashion</option>
-        <option value="food">food</option>
-        <option value="culture">culture</option>
-        <option value="travel">travel</option>
-        <option value="coding">coding</option>
-      </select>
-      <div className={styles.editor}>
-        <button className={styles.button} onClick={() => setOpen(!open)}>
-          <Image src="/plus.png" alt="" width={16} height={16} />
-        </button>
-        {open && (
-          <div className={styles.add}>
-            <input
-              type="file"
-              id="image"
-              onChange={(e) => setFile(e.target.files[0])}
-              style={{ display: "none" }}
-            />
-            <button className={styles.addButton}>
-              <label htmlFor="image">
-                <Image src="/image.png" alt="" width={16} height={16} />
-              </label>
-            </button>
-            <button className={styles.addButton}>
-              <Image src="/external.png" alt="" width={16} height={16} />
-            </button>
-            <button className={styles.addButton}>
-              <Image src="/video.png" alt="" width={16} height={16} />
-            </button>
-          </div>
-        )}
-        <ReactQuill
-          className={styles.textArea}
-          theme="bubble"
-          value={value}
-          onChange={setValue}
-          placeholder="Tell your story..."
+    typeof window !== "undefined" && (
+      <div className={styles.container}>
+        <input
+          type="text"
+          placeholder="Title"
+          className={styles.input}
+          onChange={(e) => setTitle(e.target.value)}
         />
+        <select
+          className={styles.select}
+          onChange={(e) => setCatSlug(e.target.value)}
+        >
+          <option value="style">style</option>
+          <option value="fashion">fashion</option>
+          <option value="food">food</option>
+          <option value="culture">culture</option>
+          <option value="travel">travel</option>
+          <option value="coding">coding</option>
+        </select>
+        <div className={styles.editor}>
+          <button className={styles.button} onClick={() => setOpen(!open)}>
+            <Image src="/plus.png" alt="" width={16} height={16} />
+          </button>
+          {open && (
+            <div className={styles.add}>
+              <input
+                type="file"
+                id="image"
+                onChange={(e) => setFile(e.target.files[0])}
+                style={{ display: "none" }}
+              />
+              <button className={styles.addButton}>
+                <label htmlFor="image">
+                  <Image src="/image.png" alt="" width={16} height={16} />
+                </label>
+              </button>
+              <button className={styles.addButton}>
+                <Image src="/external.png" alt="" width={16} height={16} />
+              </button>
+              <button className={styles.addButton}>
+                <Image src="/video.png" alt="" width={16} height={16} />
+              </button>
+            </div>
+          )}
+          <ReactQuill
+            className={styles.textArea}
+            theme="bubble"
+            value={value}
+            onChange={setValue}
+            placeholder="Tell your story..."
+          />
+        </div>
+        <button className={styles.publish} onClick={handleSubmit}>
+          Publish
+        </button>
       </div>
-      <button className={styles.publish} onClick={handleSubmit}>
-        Publish
-      </button>
-    </div>
+    )
   );
 };
 
