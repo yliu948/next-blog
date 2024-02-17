24b00b3647ff0953c5971699a75a57c644bf5fac
Yi Liu <justinliuyi@gmail.com>
2024-02-17 00:30:40 -0500

    update

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -66,6 +66,7 @@ import Menu from "@/components/menu/Menu";
 import styles from "./singlePage.module.css";
 import Image from "next/image";
 import Comments from "@/components/comments/Comments";
+import prisma from "@/utils/connect";
 
 const SinglePage = async ({ params }) => {
   const { slug } = params;
